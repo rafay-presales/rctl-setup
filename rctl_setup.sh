85650da7bc3b00d069023ba8ec6260c10e404d15
Kurt Madel <kurt.madel@rafay.co>
2023-06-21 16:19:20 -0400

    Create rctl_setup.sh

---
+++ rctl_setup.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+curl -sL https://s3-us-west-2.amazonaws.com/rafay-prod-cli/publish/rctl-linux-amd64.tar.bz2 > rctl.tar.bz2
+tar xvf ./rctl.tar.bz2
+chmod 755 ./rctl
+rm ./rctl.tar.bz2
+./rctl config init
+./rctl
