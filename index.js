a8095355a724a8b8a9133656597d04291f10238f
Kurt Madel <kurt.madel@rafay.co>
2023-06-21 16:55:46 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+const core = require('@actions/core');
+const tc = require('@actions/tool-cache');
+
+async function setup() {
+  // Get version of tool to be installed
+  const version = core.getInput('version');
+
+  // Download the specific version of the tool, e.g. as a tarball
+  const pathToTarball = await tc.downloadTool(getDownloadURL());
+
+  // Extract the tarball onto the runner
+  const pathToCLI = await tc.extractTar(pathToTarball);
+
+  // Expose the tool by adding it to the PATH
+  core.addPath(pathToCLI)
+}
+
+module.exports = setup
